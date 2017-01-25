84960aa9b69c6830f852420709267c66331f6f03
tal87 <talbsho@gmail.com>
2017-01-25 06:27:55 +0200

    fixed price extraction

--- index.js
+++ index.js
@@ -8,36 +8,13 @@ var app = express();
 var iconv = require('iconv-lite')
 
 var url = 'http://www.yad2.co.il/Nadlan/rent.php?City=%FA%EC+%E0%E1%E9%E1+%E9%F4%E5&Neighborhood=&HomeTypeID=&fromRooms=2&untilRooms=4&fromPrice=&untilPrice=&PriceType=1&FromFloor=&ToFloor=&EnterDate=&Info=';
-
-// app.get('/scrape', (req, res) =>{
-  // var jar = request.jar();
-  // request({ url: url, jar: jar }, (error, response, html) =>{
-  //   if(error){
-  //       console.error(error);
-  //       return;
-  //   }
-
-
-    var src = '';
-    http.get(url, (res) =>{
-        res.on('data', (chunk) =>{
-            src += iconv.decode(new Buffer(chunk), 'Windows-1255');
-        });
-
-        res.on('end', ()=>{
-            fs.writeFile('source3.txt', src);
-        });
+var src = '';
+http.get(url, (res) =>{
+    res.on('data', (chunk) =>{
+        src += iconv.decode(new Buffer(chunk), 'Windows-1255');
     });
 
-    // console.log('cookies:', jar.getCookieString(url));
-    // console.log('status', response.statusCode);
-    // console.log('headers', response.headers);
-    // fs.writeFile('source.txt', html);
-    // fs.writeFile('source.txt', iconv.decode(new Buffer(html), 'Windows-1255'));
-  // });
-
-  // res.end();
-// });
-//
-// app.listen(5000);
-// console.log('listening on port: ', 5000);
+    res.on('end', ()=>{
+        fs.writeFile('source3.txt', src);
+    });
+});
--- reader.js
+++ reader.js
@@ -5,14 +5,11 @@ fs.readFile('source2.txt', { encoding: 'utf8', flag: 'r' }, (err, html) => {
     var apts = [];
     var $ = cheerio.load(html);
     console.log('first row');
-    // console.log($('.main_table tr:not(:first-child) td:nth-child(11)').text());
-    $('.main_table tr:not(:first-child)').each((i, elem) =>{
-        // apts.push({ Price: parseFloat($(this + ' td:nth-child(11)').text()), Rooms: $(this + ' td:nth-child(13)').text() });
-        apts.push({ Price: parseFloat($(elem).find('td:nth-child(11)').text()) });
-        // console.log($(elem).find('td:nth-child(11)').text());
+    $('.main_table tr.yellow').each((i, elem) =>{
+        var priceTd = $(elem).find('td:nth-child(11)').text().trim();
+        var spaceIdx = priceTd.indexOf(' ');
+        apts.push({ Price: parseFloat(priceTd.substr(0, spaceIdx).replace(/,/g, '')), Rooms: parseInt($(elem).find('td:nth-child(13)').text()) });
     });
-    //  console.log($('.main_table tr:not(:first-child) td:nth-child(11)').text());
-
 
     console.log(apts);
 });
