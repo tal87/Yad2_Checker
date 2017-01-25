f4e4d3bd524dfbe489ddd50356d542eb7e763dbd
tal87 <talbsho@gmail.com>
2017-01-25 06:56:01 +0200

    added helper method. added url for each apartment

--- reader.js
+++ reader.js
@@ -1,6 +1,11 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
 
+function takeAfter(str, takeAfterStr){
+    var idx = str.indexOf(takeAfterStr);
+    return str.substring(idx + takeAfterStr.length);
+}
+
 fs.readFile('source2.txt', { encoding: 'utf8', flag: 'r' }, (err, html) => {
     var apts = [];
     var $ = cheerio.load(html);
@@ -8,7 +13,15 @@ fs.readFile('source2.txt', { encoding: 'utf8', flag: 'r' }, (err, html) => {
     $('.main_table tr.yellow').each((i, elem) =>{
         var priceTd = $(elem).find('td:nth-child(11)').text().trim();
         var spaceIdx = priceTd.indexOf(' ');
-        apts.push({ Price: parseFloat(priceTd.substr(0, spaceIdx).replace(/,/g, '')), Rooms: parseInt($(elem).find('td:nth-child(13)').text()) });
+        var apt = { Price: parseFloat(priceTd.substr(0, spaceIdx).replace(/,/g, '')),
+                    Rooms: parseInt($(elem).find('td:nth-child(13)').text()),
+                    Name: $(elem).find('td:nth-child(9)').text().trim(),
+                    // url: $(elem).find('td:nth-child(11)').attr('onclick')
+                    id: takeAfter($(elem).find('td:nth-child(11)').attr('onclick'), 'NadlanID\',\'').substr(0, 7),
+                  };
+
+        apt.url = 'http://www.yad2.co.il/Nadlan/rent_info.php?NadlanID=' + apt.id;
+        apts.push(apt);
     });
 
     console.log(apts);
