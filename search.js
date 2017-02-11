fe3aa0e6ef06cbe9135ae86fa63349a5e2a00cc5
tal87 <talbsho@gmail.com>
2017-02-11 08:20:38 +0200

    bug fix in get url function declaration

--- search.js
+++ search.js
@@ -1,11 +1,11 @@
 var hebrewCoder = require('./hebrew_coder.js');
-function Search(city, minRooms, maxRooms, minPrice, maxPrice) {
+function Search(minRooms, maxRooms, minPrice, maxPrice, city) {
     this.city = city;
     this.minR = minRooms;
     this.maxR = maxRooms;
     this.minP = minPrice;
     this.maxP = maxPrice;
-    this.getUrl(){
+    this.getUrl = function(){
         return '/Nadlan/rent.php?City=' + hebrewCoder.codeUrl(this.city) + '&Neighborhood=&HomeTypeID=&fromRooms=' + this.minR + '&untilRooms=' + this.maxR + '&fromPrice=' + this.minP + '&untilPrice=' + this.maxP + '&PriceType=1&FromFloor=&ToFloor=&EnterDate=&Info='
     }
 }
